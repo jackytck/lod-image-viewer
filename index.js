0c7fda6a045c9c50e45a436f1d040c2ffcd87f14
Jacky Tang <jackytck@gmail.com>
2018-11-23 17:19:55 +0800

    :sparkles: zoom to fit div

--- index.js
+++ index.js
@@ -9,8 +9,23 @@ import TileLayer from 'ol/layer/Tile'
 import XYZ from 'ol/source/XYZ'
 
 const u = new URL(window.location.href)
+
+// url
 const t = u.searchParams.get('url')
 const url = `${t}/{z}/{x}/{y}.jpg`
+
+// ppt: pixel per tile, default 256
+const ppt = +u.searchParams.get('ppt') || 256
+
+// mz, default 5
+const maxZoom = +u.searchParams.get('mz') || 4
+
+// zoom to fit window
+const mapDiv = document.getElementById('map')
+const divWidth = mapDiv.clientWidth
+const divHeight = mapDiv.clientHeight
+const shortSize = Math.min(divWidth, divHeight)
+const fitZoom = Math.min(maxZoom, Math.floor(Math.log(shortSize / ppt) / Math.log(2)))
 
 const map = new Map({
   target: 'map',
@@ -24,7 +39,8 @@ const map = new Map({
   ],
   view: new View({
     center: [0, 0],
-    zoom: 0
+    zoom: fitZoom,
+    maxZoom
   })
 })
 
